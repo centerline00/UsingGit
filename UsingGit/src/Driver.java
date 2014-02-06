ad3ec5951ec0abecd5ddcebfb41f2fa2de90173c
Stephanie <Stephanie@192.168.1.104>
2014-02-06 15:22:12 -0500

    first saved commit, system.out.println statement of first saved commit.

--- UsingGit/src/Driver.java
+++ UsingGit/src/Driver.java
@@ -4,5 +4,6 @@ public class Driver {
 	public static void main(String[] args) {
 		DoSomething ds = new DoSomething();
 		System.out.println(ds.printer("Hello there implemented interface class."));
+		System.out.println("commited to Git for first time");
 	}
 }
